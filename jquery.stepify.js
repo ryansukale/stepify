c50a2d41d8a70fce5ace2beb3a920bb30e9cf5a2
Ryan Sukale <ryansukale@gmail.com>
2013-10-08 17:20:10 -0400

    Divided elements into steps

--- jquery.stepify.js
+++ jquery.stepify.js
@@ -23,15 +23,30 @@
 		
 		var prevBtnStr = '<div class="'+settings.prevBtnClass+'">'+settings.prevBtnText+'</div>';
 		var nextBtnStr = '<div class="'+settings.nextBtnClass+'">'+settings.nextBtnText+'</div>';
+		var divStr = '<div></div>';
 		
 		var stepContainers = 0;
+		var $originalSet = this;
+		var $parent = $originalSet.parent();
 		
-		$.each(settings.distribution, function(index, elem){
+		var currentElement = 0;
+		
+		$.each(settings.distribution, function(index, numberOfElements){
 			
 			var $btnContainer = $(btnContainerStr);
 			var $stepContainer = $(stepContainerStr);
+			var $div = $(divStr);
 			
 			$btnContainer.append(prevBtnStr + nextBtnStr);
+			
+			for(var i=0;i<numberOfElements;i++){
+				$div.append($originalSet[currentElement]);
+				console.log(currentElement);
+				currentElement++;
+			}
+			
+			
+			$stepContainer.append($div);
 			$stepContainer.append($btnContainer);
 			
 			//Set the id of the stepContainer
@@ -39,6 +54,9 @@
 			stepContainers = stepContainers+1;
 			
 			console.log($stepContainer);
+			
+			$parent.append($stepContainer);
+			
 			
 		});
 		
